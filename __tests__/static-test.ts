1ec62083e8664f44a9e95783eb9e5082f62887c5
Eric Prud'hommeaux <eric+github@w3.org>
2021-07-18 05:17:38 +0200

    ~ split out shapePathQuery function

--- __tests__/static-test.ts
+++ __tests__/static-test.ts
@@ -3,13 +3,13 @@ const Path = require('path')
 import { stringFacet, TripleConstraint } from 'shexj'
 import { EvalContext, NodeSet, SchemaNode, stringFacetAttr } from '../src/ShapePathAst'
 import { ShapePathParser, ShapePathLexer } from '../src/ShapePathParser'
-import { Schema } from 'shexj'
+import { Schema, SemAct } from 'shexj'
 
 // For validation tests
 const ShExValidator = require('@shexjs/validator')
 const ShExUtil = require('@shexjs/util')
 const ShExTerm = require('@shexjs/term')
-const ShExMap = require('@shexjs/map')
+const ShExMap = require('@shexjs/extension-map')
 const MapModule = ShExMap(ShExTerm)
 import { Store as RdfStore, Parser as TurtleParser } from 'n3'
 
@@ -275,36 +275,46 @@ describe('selection/validation tests', () => {
           , 'utf8'
         )
       ))
-
-      // Add ShExMap annotations to each element of the nodeSet.
-      // ShExMap binds variables which we use to capture schema matches.
-      const vars = nodeSet.map((shexNode) => {
-        const varName = 'http://a.example/var' + nodeSet.indexOf(shexNode);
-        // Pretend it's a TripleConstraint. Could be any shapeExpr or tripleExpr.
-        (shexNode as TripleConstraint).semActs = [{
-          "type": "SemAct",
-          "name": MapModule.url,
-          "code": `<${varName}>`
-        }]
-        return varName
-      })
-
-      // Construct validator with ShapeMap semantic action handler.
-      const validator = ShExValidator.construct(schema, ShExUtil.rdfjsDB(graph), {});
-      const mapper = MapModule.register(validator, { ShExTerm })
-
-      // Expect successful validation.
-      const shape = valTest.action.shape || ShExValidator.start
-      const node = valTest.action.focus
-      const valRes = validator.validate([{ node, shape }])
-      expect(valRes.errors).toBeUndefined
-
-      // Compare to reference.
-      const resultBindings = ShExUtil.valToExtension(valRes, MapModule.url);
-      expect(vars.map(v => resultBindings[v])).toEqual(entry.shapePathDataMatch)
+      const db = ShExUtil.rdfjsDB(graph)
+      const queryResults = shapePathQuery(
+        schema,
+        nodeSet,
+        db,
+        valTest.action.focus,
+        valTest.action.shape || ShExValidator.start
+      );
+      expect(queryResults).toEqual(entry.shapePathDataMatch)
     })
   )
 })
+
+function shapePathQuery (schema: Schema, nodeSet: NodeSet, db: any, node: any, shape: any) {
+  // Add ShExMap annotations to each element of the nodeSet.
+  // ShExMap binds variables which we use to capture schema matches.
+  const vars = nodeSet.map((shexNode) => {
+    const varName = 'http://a.example/var' + nodeSet.indexOf(shexNode)
+    // Pretend it's a TripleConstraint. Could be any shapeExpr or tripleExpr.
+    const varAssignSemAct: SemAct = {
+      type: 'SemAct',
+      name: MapModule.url,
+      code: `<${varName}>`
+    };
+    (shexNode as TripleConstraint).semActs = [varAssignSemAct];
+    return varName
+  })
+
+  // Construct validator with ShapeMap semantic action handler.
+  const validator = ShExValidator.construct(schema, db, {})
+  const mapper = MapModule.register(validator, { ShExTerm })
+
+  // Expect successful validation.
+  const valRes = validator.validate([{ node, shape }])
+  expect(valRes.errors).toBeUndefined
+
+  // Compare to reference.
+  const resultBindings = ShExUtil.valToExtension(valRes, MapModule.url)
+  return vars.map(v => resultBindings[v])
+}
 
 function parseShapePathFile(filename: string): object {
   const txt = Fs.readFileSync(Path.join(__dirname, filename), 'utf8')
